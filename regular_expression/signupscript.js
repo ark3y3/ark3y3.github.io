3ce2f6b7094b1f99e67ad0a4cbd5612bf6e94398
Okke <kuroxy.mail@gmail.com>
2023-01-23 16:23:23 +0100

    Co-authored-by: wishaal <ark3y3@users.noreply.github.com>

---
+++ regular_expression/signupscript.js
@@ -0,0 +1,16 @@
+
+        
+
+        var clicks = 0;
+        var click = document.getElementById("body");
+        click.addEventListener('click', function() {
+            clicks++;
+            console.log("clicks: "+clicks)
+        })
+
+        var keypresses = 0;
+        var keys = document.getElementById("form");
+        keys.addEventListener('keypress', function() {
+            keypresses++;
+            console.log("keypresses: "+keypresses);
+        });
