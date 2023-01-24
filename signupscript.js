ee0a034a6ed510619d96925af84476ee9f01d4ce
wishaal <wrkanhai@gmail.com>
2023-01-24 17:09:16 +0100

    Show time on submit

--- signupscript.js
+++ signupscript.js
@@ -26,6 +26,11 @@ function showStats(){
     var deleteDiv = document.getElementById("stats");
     deleteDiv.remove();
 
+    seeTime();
+    startDate = new Date();
+    const minutes = Math.floor(spentTime/60);
+    const seconds = spentTime%60;
+
     const newDiv = document.createElement("div");
     newDiv.setAttribute("id", "stats");
     body.appendChild(newDiv);
@@ -33,13 +38,36 @@ function showStats(){
     const stats = document.createElement("span");
     const clix = document.createTextNode("clicks: "+clicks);
     const keyz = document.createTextNode("keypresses: "+keypresses);
+    const time = document.createTextNode("time spent: "+minutes+" minutes and "+seconds+" seconds");
 
     stats.appendChild(clix);
     stats.appendChild(document.createElement("br"));
     stats.appendChild(keyz);
+    stats.appendChild(document.createElement("br"));
+    stats.appendChild(time);
     newDiv.appendChild(stats);
 
     clicks = 0;
     keypresses = 0;
 }
+
+
+let startDate = new Date();
+let spentTime = 0;
+
+const focus = function() {
+    startDate = new Date();
+};
+
+window.addEventListener('focus', focus);
+
+const seeTime = function() {
+    const endDate = new Date();
+    spentTime = endDate.getTime() - startDate.getTime();
+    spentTime = Math.round(spentTime/1000);
+    console.log(spentTime);
+    // elapsedTime contains the time spent on page in milliseconds
+};
+
+
 
