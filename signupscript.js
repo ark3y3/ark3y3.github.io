4d1343d5ed65f5d732e86189a3d609f3672a3075
wishaal <wrkanhai@gmail.com>
2023-01-24 16:57:00 +0100

    signup will show clicks + keypresses

--- signupscript.js
+++ signupscript.js
@@ -1,16 +1,45 @@
 
-        
+var body = document.getElementById("body");
 
-        var clicks = 0;
-        var click = document.getElementById("body");
-        click.addEventListener('click', function() {
-            clicks++;
-            console.log("clicks: "+clicks)
-        })
 
-        var keypresses = 0;
-        var keys = document.getElementById("form");
-        keys.addEventListener('keypress', function() {
-            keypresses++;
-            console.log("keypresses: "+keypresses);
-        });
+var clicks = 0;
+var click = document.getElementById("body");
+click.addEventListener('click', function() {
+    clicks++;
+    console.log("clicks: "+clicks)
+})
+
+var keypresses = 0;
+var keys = document.getElementById("form");
+keys.addEventListener('keypress', function() {
+    keypresses++;
+    console.log("keypresses: "+keypresses);
+});
+
+function onSubmit() {
+    var frm = document.getElementById("form");
+    frm.reset();
+    showStats();
+}
+
+function showStats(){
+    var deleteDiv = document.getElementById("stats");
+    deleteDiv.remove();
+
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "stats");
+    body.appendChild(newDiv);
+
+    const stats = document.createElement("span");
+    const clix = document.createTextNode("clicks: "+clicks);
+    const keyz = document.createTextNode("keypresses: "+keypresses);
+
+    stats.appendChild(clix);
+    stats.appendChild(document.createElement("br"));
+    stats.appendChild(keyz);
+    newDiv.appendChild(stats);
+
+    clicks = 0;
+    keypresses = 0;
+}
+
